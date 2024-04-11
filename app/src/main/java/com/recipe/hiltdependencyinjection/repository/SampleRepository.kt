4b8996ed4e54db6aae55e7abb81dbadcbc7ab7c2
Rohit Kumar <>
2024-04-11 17:26:01 +0530

    clean up

--- app/src/main/java/com/recipe/hiltdependencyinjection/repository/SampleRepository.kt
+++ app/src/main/java/com/recipe/hiltdependencyinjection/repository/SampleRepository.kt
@@ -5,7 +5,6 @@ import com.recipe.hiltdependencyinjection.interfaces.SampleInterface
 import com.recipe.hiltdependencyinjection.manager.SampleManager
 import com.recipe.hiltdependencyinjection.module.IM2
 import javax.inject.Inject
-import javax.inject.Named
 
 class SampleRepository @Inject constructor(private val sampleManager: SampleManager,@IM2 private val sampleInterface: SampleInterface) {
     fun callApi() {
