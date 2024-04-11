c716407554b49c27b0aebd91eb2f07ede355fe8e
Rohit Kumar <>
2024-04-11 13:57:37 +0530

    initial commit

---
+++ app/src/main/java/com/recipe/hiltdependencyinjection/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.recipe.hiltdependencyinjection
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
